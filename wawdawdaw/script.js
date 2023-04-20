5263922659585bca94e2867e488b2b7664762f31
FranzSchubertFr <111022277+FranzSchubertFr@users.noreply.github.com>
2023-04-21 01:18:47 +0200

    Add files via upload

---
+++ wawdawdaw/script.js
@@ -0,0 +1,15 @@
+const learn_more = document.getElementById("learn-more");
+const sound = new Audio("AUUGHHH.mp3");
+const closePopup = document.getElementById("closePopup");
+const myPopup = document.getElementById("myPopup");
+
+learn_more.addEventListener("click", function() {
+    sound.play();
+    myPopup.style.display = "block";
+    sound.play();
+    sound.play();
+});
+
+closePopup.addEventListener("click", function() {
+    myPopup.style.display = "none";
+  });
